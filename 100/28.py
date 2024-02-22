e5618dc81140c6582b9b3fd3cb0fa358e714bad9
rkawkclzls <38097671+rkawkclzls@users.noreply.github.com>
2024-02-22 23:38:35 +0900

    28번 풀이

--- 100/28.py
+++ 100/28.py
@@ -1,4 +1,18 @@
 data = ([1, 2, 3, 4, 5], 10)
 
 data,s = data
+data = data * 2
+
+a = [data[i:i+2] for i in data]
+
+print(a)
+b = [j[1] + j[0] for j in a] 
+
+b = list(set(b))
+c = [abs(s - k) for k in b]
+
+d = list(zip(b,c))
+d = sorted(d, key= lambda x:x[1])
+print(d[0][0])
+
 
